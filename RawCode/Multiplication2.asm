bb629ad2653afbf8ec1b039c81254dd695b50ec4
Ken <andersonkennethw@outlook.com>
2025-03-20 08:27:56 -0400

    Update Multiplication2.asm

--- RawCode/Multiplication2.asm
+++ RawCode/Multiplication2.asm
@@ -5,31 +5,31 @@
 
   .org $C000
 
-  lda #$07 ; Multiplicand Value
+  lda #$07  ; Multiplicand Value
   sta $F0
-  lda #$4A ; Multiplier Value
+  lda #$4A  ; Multiplier Value
   sta $F1
 
-  lda #$00 ; clear accumulator
-  ldx #$00 ; clear x
+  lda #$00  ; clear accumulator
+  ldx #$00  ; clear x
   ldy #$00
 
 addLoop:
-  clc      ; clear carry
-  adc $F0  ; add multiplicand
-  bcs overflowCalculation ; to on carry
+  clc                      ; clear carry
+  adc $F0                  ; add multiplicand
+  bcs overflowCalculation  ; to on carry
 contLoop:
-  inx      ; inc x
-  cpx $F1  ; compare x to multiplier
-  bne addLoop ; if not equal : addLoop
+  inx            ; inc x
+  cpx $F1        ; compare x to multiplier
+  bne addLoop    ; if not equal : addLoop
 
-  jmp endSave
+  jmp endSave    ; skip overflow calculation at the end
 
 overflowCalculation:
-  iny
-  jmp contLoop
+  iny            ; inc y for high byte
+  jmp contLoop   ; Restart addition loop
 
 endSave:
-  sta $F3
-  sty $F2
+  sta $F3  ; Store low-byte
+  sty $F2  ; Store high-byte
   brk
