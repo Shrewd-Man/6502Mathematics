8ffd9d0f87b8957387dab60feaf9b8a6af09f773
Ken <andersonkennethw@outlook.com>
2025-03-20 08:25:31 -0400

    Update Division1.asm

--- RawCode/Division1.asm
+++ RawCode/Division1.asm
@@ -6,25 +6,25 @@
 
   .org $C000
 
-  lda #$1B ; Dividend Value
+  lda #$1B    ; Dividend Value
   sta $F0
 
-  lda #$03 ; Divisor Value
+  lda #$03    ; Divisor Value
   sta $F1
 
-  ldx #$00 ; Set X to 0 to prepare
-  lda $F0 ; load dividend
+  ldx #$00    ; Set X to 0 to prepare
+  lda $F0     ; load dividend
 
 subLoop:
   cmp $F1
-  bcc doneDivide ; If dividend < divisor, we're done
+  bcc doneDivide  ; If dividend < divisor, we're done
 
-  sec ; set carry for accurate sub
-  sbc $F1 ; subtract divisor
-  inx ; increment x
-  jmp subLoop ; restart until done
+  sec          ; set carry for accurate sub
+  sbc $F1      ; subtract divisor
+  inx          ; increment x
+  jmp subLoop  ; restart until done
 
 doneDivide:
-  sta $F3 ; store a as remainder
-  stx $F2 ; store x as result
+  sta $F3   ; store a as remainder
+  stx $F2   ; store x as result
   brk
